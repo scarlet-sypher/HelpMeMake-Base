30ade2a9e52ec7c608e0ea3f659cc29d03b6f152
RoHobie <ronitthakur267@gmail.com>
2025-07-15 14:07:47 +0530

    FIX: added and fixed Routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Login from './Login';
+import Signup from './Signup';
+import UserDashboard from './userDashboard';
+import MentorDashboard from './mentorDashboard';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/userdashboard" element={<UserDashboard />} />
+        <Route path="/mentordashboard" element={<MentorDashboard />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
