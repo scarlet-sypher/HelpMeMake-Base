8aa0a371fb16c5682d4b48b2ba788f72fc022b97
OmSingh04 <omsingh18609@gmail.com>
2025-07-20 16:41:08 +0530

    adding quick actions with carsoul effect in mentor dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import StatusGrid from "./StatusGrid";
+import Quick from "./Quick";
 function Dashboard(){
     return (
         <div className="space-y-6">
             {/* Status grid */}
             < StatusGrid />
+
+            {/* {Quick-Actions} */}
+
+            <Quick />
             
         </div>
     )
