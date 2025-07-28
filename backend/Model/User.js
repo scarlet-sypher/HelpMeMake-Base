c26f0dc61d17f4471900aadff5a07884b7b1eb98
Ayush <ayushjha21157@gmail.com>
2025-07-28 18:55:52 +0530

    Modified: added github field in user model

--- backend/Model/User.js
+++ backend/Model/User.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     sparse: true
   },
+  githubId: {
+    type: String,
+    sparse: true
+  },
   name: {
     type: String,
     trim: true
