562b52956c69bcc73a67277cebbf7257de746c53
Ayush <ayushjha21157@gmail.com>
2025-07-28 19:00:54 +0530

    Modified: added github Strategy in Passwport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,10 +1,12 @@
 const passport = require('passport');
 const googleStrategy = require('./strategies/google-login');
 const jwtStrategy = require('./strategies/jwt');
+const githubStrategy = require('./strategies/github-login');
 
 
 passport.use('google', googleStrategy);
 passport.use('jwt', jwtStrategy);
+passport.use('github', githubStrategy);
 
 
 
